666fa15f031c4c6ebec23fb9dd980380674ce188
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-05-03 10:56:33 +0530

    show all districts and pincodes on screen

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const stateSelector = document.querySelector("select");
 const messageElement = document.getElementById("message");
+const districtListElement = document.getElementById("district-list");
+const pincodeListElement = document.getElementById("pincode-list");
 
 const BASE_URL = "https://api.data.gov.in/resource/5c2f62fe-5afa-4119-a499-fec9d604d5bd"
 const KEY = "579b464db66ec23bdd00000146920f2ff2a444c973bcc2ee67a1dfe8";
@@ -44,4 +46,12 @@ stateSelector.addEventListener("change", async (e) => {
     };
 
     messageElement.innerText = `${state} has ${totalPincodes.size} pincodes and ${totaDistricts.size} districts.`;
-})
+
+    for (const district of totaDistricts.values()) {
+        districtListElement.innerHTML += `<li>${district}</li>`
+    }
+
+    for (const pincode of totalPincodes.values()) {
+        pincodeListElement.innerHTML += `<li>${pincode}</li>`
+    }
+});
